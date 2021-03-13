3e33f450725b3906083e052d8b15126352f4db1a
hellonathapon <hellonathapon@gmail.com>
2021-03-13 09:33:49 +0700

    Add cookie-session

--- app.js
+++ app.js
@@ -3,13 +3,23 @@ const app = express();
 const port = process.env.PORT || 5000;
 const passport = require('passport');
 const passportConfig = require('./configs/passport');
+const cookieSession = require('cookie-session');
+require('dotenv').config();
 
 // set up view engine
 app.set('view engine', 'ejs');
 
 // serve static file
 app.use(express.static("public"));
+
+// set up cookie
+app.use(cookieSession({
+    maxAge: 24 * 60 * 60 * 1000,
+    keys: [process.env.COOKIE_SESSION_KEY],
+}))
+
 app.use(passport.initialize());
+app.use(passport.session());
 
 // handle routes
 app.use('/auth', require('./routes/auth/index.js')); // route position order is important
--- configs/passport.js
+++ configs/passport.js
@@ -2,6 +2,15 @@ const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
 require('dotenv').config();
 
+passport.serializeUser(function(user, done){
+    done(null, user.id)
+});
+
+passport.deserializeUser(function(id, done){
+    // use id to find user in DB and return done with user data pass along
+    // done(null, user)
+})
+
 passport.use( new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
@@ -12,13 +21,5 @@ passport.use( new GoogleStrategy({
     console.log(profile)
     done(null, profile)
 }));
-
-passport.serializeUser(function(user, done){
-    done(null, user.id)
-});
-
-passport.deserializeUser(function(id, done){
-    done(null, id)
-})
 
 module.exports = passport;
